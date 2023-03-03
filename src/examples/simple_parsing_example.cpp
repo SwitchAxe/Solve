133f0495f6ad8be6bc30b23e10295e0c0c135c75
Sofia <sofiacerasuoli@gmail.com>
2023-03-03 12:34:06 +0100

    Added a parsing example

--- src/examples/simple_parsing_example.cpp
+++ src/examples/simple_parsing_example.cpp
@@ -2,6 +2,6 @@
 #include "parser.hpp"
 
 int main() {
-  solve::parser::print_tree(
-      solve::parser::parse(solve::lexer::tokens("Foo[1, Bar[4, 5, 6], 3]")));
+  solve::parser::print_tree(solve::parser::parse(
+      solve::lexer::tokens("Foo[1 / 2 * 2, 3 + 4 - 5, Bar[1, 2, 3]]")));
 }
